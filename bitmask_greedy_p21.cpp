f312e86b0fdc2f1bbf0da270ee9f46fe7beb489f
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2025-06-08 09:01:30 +0530

    p21

--- bitmask_greedy_p21.cpp
+++ bitmask_greedy_p21.cpp
@@ -13,27 +13,23 @@ unordered_map<int, int> hasMask;
 for (int i = 0; i < n; i++) {
 		if (!ok[i]) continue;
 		for (int j = i + 1; j < n; i++) {
-			// make all the pair mask
 			if (!ok[j]) continue;
 			if (mask[i] & mask[j]) continue;
+			for (int k = j + 1; j < n; j++) {
+				if ((mask[i] & mask[j]) & mask[k]) continue;
+				int curMask = (mask[i] | mask[j])  mask[k];
+				int maskNeeded = (1 << 26) - (1 << (ch - 'a')) - curMask;
+				if (hasMask[maskNeeded]) {
+
+				}
+			}
+		}
+
+		for (int j = i - 1; j >= 0; j--) {
+			if (!ok[j] || (mask[i] | mask[j]) > 0) continue;
 			hasMask[mask[i] | mask[j]]++;
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		if (!ok[i]) continue;
-		// first removed all the pair with i-th string
-		for (int j = i + 1; j < n; i++) {
-			if (!ok[j]) continue;
-			if (mask[i] & mask[j]) continue;
-			hasMask[mask[i] | mask[j]]--;
-		}
-		for (int j = i - 1; j >= 0; j--) {
-			if (!ok[j]) continue;
-			if (mask[i] & mask[j]) continue;
-			int remMask = (1 << 26) - (1 << (ch - 'a')) - (mask[i] | mask[j]);
-			if (hasMask[remMask]) {
-				return true;
-			}
-		}
-	}
 }
+
+
