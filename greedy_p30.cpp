8decb10b0f8009d83cdcaf71afb782a848a39cb4
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2025-06-17 08:17:51 +0530

    greedy

---
+++ greedy_p30.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/discuss/post/6789894/l4-google-question-by-anonymous_user-pork/
+
+int solve(const vector<int>& balls, const int k) {
+    vector<int> cnt(k);
+    for (const auto& b : balls)
+        ++cnt[b];
+    int used = 0;
+    vector<int> left;
+    for (const auto& c : cnt) {
+        used += c / k;
+        if (c % k != 0)
+            left.push_back(c % k);
+    }
+    sort(left.begin(), left.end(), greater());
+    int r = (int) left.size(), opt = r;
+    for (int i = 0; i < r; i++) {
+        opt = min(opt, i + max(left[i], sum(left[i] + left[i + 1] + .. + left[r - 1]) / k);
+    }
+          return used + opt;
+}
