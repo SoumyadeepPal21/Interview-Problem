36a4fa416336543aea1aa85057b804b1d7f15571
SoumyadeepPal21 <soumyadeepbappipal@gmail.com>
2025-10-05 11:14:03 +0530

    fahl

--- binary_search.cpp
+++ binary_search.cpp
@@ -37,6 +37,9 @@ in case of sparse array(less number of 1's) we do K * Log(N) operations.
                             (2 ^ log(N + 1) - 1) / (2 - 1) = 2 * N + 1
 
 
+                            space complexity: O(log(N) + K)
+
+
 
 
                             // iterative solution
@@ -63,3 +66,62 @@ in case of sparse array(less number of 1's) we do K * Log(N) operations.
                             start = pos + 1;
                         }
 
+                            //  this solution will take O(K * Log(N)) time
+                            // space = O(1 + K)
+
+
+
+
+                            2. https://leetcode.com/problems/separate-squares-i/solutions/
+
+                            In a x-y cartesian plane , squares are made with axes paralled to x-axis and y- axis, their topleft corner x,y is given and edge length a is given for each,,
+                            we have to tell a horizontal axis that divides the area of all squares in 2 equal halves.
+                            l=[(1,2),(2,4)] //topleft coordinates
+                            sizes=[5,10] //edge lengths
+
+                            one question => if there are multiple horizontal axis, then what to do? find the minimum ?
+
+
+                            double L = min(y), R = max(Y + L)
+
+                            while (R - L > 1e-9) {
+                            A = findAreAbove(mid)
+                            if (Total > A * 2) {
+                            L = mid;
+                        } else {
+                            R = mid;
+                        }
+                        }
+
+                            return (L + R) / 2;
+
+
+                            3. https://leetcode.com/discuss/post/4102630/google-interview-question-by-anonymous_u-ffbi/
+
+
+                            vector<int> v = {1, 2, .., n}
+
+                            while (v.size() > 2) {
+                            vector<int> v1, v2;
+                            for (int i = 0; i < v.size() / 2; i++) v1.push_back(v[i]);
+                            for (int i = v.size() / 2 + 1; i < v.size(); i++) v1.push_back(v[i]);
+
+                            if (query(v1) == fail) {
+                            v = v1;
+                        } else if (query(v2) == fail) {
+                            v = v2;
+                        } else {
+                            v1_1, v1_2, v2_1, v2_1 (partion v1, v2 into two parts)
+                            if (query(v1_1 + v2_1) == fail) {
+                            v = v1_1 + v2_1;
+                        } else if (query(v1_2 + v2_1) == fail) {
+                            v = v1_2 + v2_1;
+                        } else if (query(v1_1 + v2_2) == fail) {
+                            v = v1_1 + v2_2;
+                        } else {
+                            v = v1_2 + v2_2;
+                        }
+                        }
+                        }
+
+                            T(n) = T(n / 2) + N => O(2 * N)
